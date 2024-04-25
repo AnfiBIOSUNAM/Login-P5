9597ef2a81ff8c08c1a467e4f3a26b233a3d3053
jatziri2 <91804499+jatziri2@users.noreply.github.com>
2024-04-25 14:16:03 -0600

    cambios en ControllerUsuario.py

--- app-flask/controller/ControllerUsuario.py
+++ app-flask/controller/ControllerUsuario.py
@@ -48,7 +48,7 @@ def login():
         user = mu.find_user_by_email_and_password(correo, contraseña)
 
         if user == -1:
-            return json.dumps({'error': 'usuario_inexistente'})
+            return json.dumps({'error': 'usuario_incorrecto'})
         elif user == -2:
             return json.dumps({'error': 'contraseña_incorrecta'})
 
