e63909e462e62550d19b36f3dacae055c3a10d85
IsaacAE <alcantara_estrada_kevin@ciencias.unam.mx>
2024-04-25 01:38:17 -0600

    Agregando bosquejo del .css

--- app-react/src/components/Pages/HomeUser/HomeUser.jsx
+++ app-react/src/components/Pages/HomeUser/HomeUser.jsx
@@ -1,8 +1,11 @@
+import './HomeUser.css'
 export default function HomeUser() {
     return (
         <>
+            <div className="fullscreen-shape"></div>
             <h1>Home</h1>
             <p>Bienvenido a Prometienda</p>
         </>
     )
 }
+
